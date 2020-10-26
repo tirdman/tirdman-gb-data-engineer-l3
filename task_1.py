b74860e257727aa45b210971ccaa67d5082b2a1e
tirdman <tirdman@outlook.com>
2020-10-26 09:30:29 +0700

    task 1 is finished

--- task_1.py
+++ task_1.py
@@ -6,3 +6,30 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+
+script_name, hours_in, rate_in, bonus_in = argv
+
+
+def salary_calc(hours, rate, bonus):
+    """
+    Функция расчета заработанной платы
+    :param hours: отработанные часы
+    :param rate: ставка
+    :param bonus: премия
+    :return: возвращает расчитанную заработанную плату
+    """
+    try:
+
+        if float(hours) < 0 or float(rate) < 0 or float(bonus) < 0:
+            print('Все введенные значения не должны быть отрицательными')
+            return
+
+        return (float(hours) * float(rate)) + float(bonus)
+    except ValueError:
+        print('Ошибка. Все введенные значения должны быть цифрами')
+
+
+salary = salary_calc(hours_in, rate_in, bonus_in)
+print(f"Заработная плата = {salary}") if salary else print(f"Расчет не выполнен")
--- tests.py
+++ tests.py
@@ -1,1 +1,33 @@
+from math import fsum
+#
+# from django import request
+
 print('Test')
+
+
+
+
+def generator():
+    for el in (1, 2, 3):
+        yield el
+
+def generator2():
+    g32 = (el for el in (1, 2, 3))
+    return g32
+
+
+g = generator()
+
+for i in g:
+    print(i)
+
+print('---------')
+
+g2 = generator2()
+next(g2)
+
+
+for i in g2:
+    print(i)
+
+[]
